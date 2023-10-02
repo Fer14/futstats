8bc62e4716d1489ba8de62e6536ddbdbfb08e169
Fer14 <fcsancho14@gmail.com>
2023-10-02 23:38:11 +0200

    update utils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="futstats",
+    version="1.0",
+    description="Python Distribution Utilities",
+    author="Fernando",
+    packages=find_packages(),
+)
