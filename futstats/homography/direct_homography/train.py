a8176fe25da391762cabf6cb5c7ce6fb039d6c91
Fer14 <fcsancho14@gmail.com>
2023-10-14 22:34:54 +0200

    update

--- futstats/homography/direct_homography/train.py
+++ futstats/homography/direct_homography/train.py
@@ -1,16 +1,29 @@
 import glob
 import os
 
+import re
+
 import cv2
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications.efficientnet_v2 import preprocess_input
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.layers import Dense, Flatten, Reshape
+from tensorflow.keras.layers import (
+    Input,
+    Dense,
+    Flatten,
+    Reshape,
+    Conv2D,
+    MaxPooling2D,
+    Flatten,
+    Concatenate,
+    Dense,
+)
 from tensorflow.keras.models import Model
 from keras.utils import Sequence
 import typer
+import scipy
 
 if tf.test.gpu_device_name():
     print("Default GPU Device: {}".format(tf.test.gpu_device_name()))
@@ -44,14 +57,15 @@ class MatrixErrorMetrics:
 
 
 class HomographyModel:
-    def __init__(self, input_shape=(540, 540, 3)):
+    def __init__(self, double_input, input_shape=(540, 540, 3)):
         self.input_shape = input_shape
-        self.model = self._build_model()
         self.metrics = [
             MatrixErrorMetrics.mean_squared_error_matrix,
             MatrixErrorMetrics.mean_absolute_error_matrix,
             MatrixErrorMetrics.root_mean_squared_error_matrix,
         ]
+        self.double_input = double_input
+        self.model = self._build_model()
 
     def _build_model(self):
         pretrained_model = tf.keras.applications.EfficientNetV2S(
@@ -60,22 +74,31 @@ class HomographyModel:
         for layer in pretrained_model.layers:
             layer.trainable = True
 
-        flatten_layer = Flatten()(pretrained_model.output)
+        image_flatten_layer = Flatten()(pretrained_model.output)
 
-        dense_layer_1 = Dense(36, activation="relu")(flatten_layer)
+        if self.double_input:
+            mask_input = Input(shape=(self.input_shape[0], self.input_shape[1], 1))
+            mask_cnn = Conv2D(64, (3, 3), activation="relu")(mask_input)
+            mask_cnn = MaxPooling2D((2, 2))(mask_cnn)
+            mask_output = Flatten()(mask_cnn)
+            input_layer = Concatenate()([image_flatten_layer, mask_output])
+            inputs_ = [pretrained_model.input, mask_input]
+        else:
+            input_layer = image_flatten_layer
+            inputs_ = pretrained_model.input
+
+        dense_layer_1 = Dense(36, activation="relu")(input_layer)
         dense_layer_2 = Dense(18, activation="relu")(dense_layer_1)
         dense_layer = Dense(9, activation="relu")(dense_layer_2)
         output_layer = Reshape((3, 3))(dense_layer)
 
-        input_layer = pretrained_model.input
         model = Model(
-            inputs=input_layer,
+            inputs=inputs_,
             outputs=output_layer,
         )
         return model
 
     def train(self, epochs: int, batch_size: int, train_dataset, validation_dataset):
-
         self.model.compile(
             optimizer="adam",
             loss="mean_squared_error",
@@ -101,7 +124,6 @@ class HomographyModel:
         )
 
     def __call__(self, input_img):
-
         image = preprocess_input(
             cv2.resize(
                 cv2.cvtColor(cv2.imread(input_img), cv2.COLOR_BGR2RGB),
@@ -112,16 +134,16 @@ class HomographyModel:
 
 
 class HomographyDataset(Sequence):
-    def __init__(self, images_paths, labels_list, image_size, batch_size):
-
-        self.images_paths = images_paths
+    def __init__(self, input_paths, labels_list, double_input, image_size, batch_size):
+        self.input_paths = input_paths
         self.labels_list = labels_list
         self.batch_size = batch_size
         self.image_size = image_size
-        self.indices = np.arange(len(images_paths))
+        self.double_input = double_input
+        self.indices = np.arange(len(labels_list))
 
     def __len__(self):
-        return int(np.ceil(len(self.images_paths) / self.batch_size))
+        return int(np.ceil(len(self.input_paths) / self.batch_size))
 
     def _read_homography(self, file_path: str):
         homography_matrix = []
@@ -135,54 +157,109 @@ class HomographyDataset(Sequence):
 
         return np.array(homography_matrix).reshape(3, 3)
 
+    def _read_mat_madk(self, file_path: str):
+        mat_contents = scipy.io.loadmat(file_path)
+        return mat_contents["grass"]
+
     def __getitem__(self, index):
         start = index * self.batch_size
         end = (index + 1) * self.batch_size
         indices = self.indices[start:end]
 
-        X = []
+        if self.double_input:
+            X_images = []
+            X_masks = []
+        else:
+            X = []
         y = []
 
         for i in indices:
-            image = preprocess_input(
-                cv2.resize(
-                    cv2.cvtColor(cv2.imread(self.images_paths[i]), cv2.COLOR_BGR2RGB),
-                    self.image_size,
+            if self.double_input:
+                image = preprocess_input(
+                    cv2.resize(
+                        cv2.cvtColor(
+                            cv2.imread(self.input_paths[i][0]), cv2.COLOR_BGR2RGB
+                        ),
+                        self.image_size,
+                    )
                 )
-            )
+                X_images.append(image)
+                mask = self._read_mat_madk(self.input_paths[i][1])
+                X_masks.append(mask)
+            else:
+                image = preprocess_input(
+                    cv2.resize(
+                        cv2.cvtColor(
+                            cv2.imread(self.input_paths[i]), cv2.COLOR_BGR2RGB
+                        ),
+                        self.image_size,
+                    )
+                )
+                X.append(image)
+
             matrix = self._read_homography(self.labels_list[i])
-            X.append(image)
             y.append(matrix)
 
-        return np.array(X), np.array(y)
+        if self.double_input:
+            output = np.array(X_images), np.array(X_masks), np.array(y)
+        else:
+            output = np.array(X), np.array(y)
+
+        return output
 
 
-def main(train: bool = True):
+def get_number_from_string(s):
+    s = s.split("/")[-1]
+    match = re.search(r"\d+", s)
+    if match:
+        return int(match.group())
+    else:
+        return 0  # If no number is found, return 0
 
+
+def main(train: bool = True, double_input: bool = True):
     DATA_DIR = "/home/fer/Escritorio/futstatistics/datasets/dataset7_homography"
     BATCH_SIZE = 8
 
     images_1 = glob.glob(os.path.join(DATA_DIR, "test") + "/*.jpg")
     images_2 = glob.glob(os.path.join(DATA_DIR, "train_val") + "/*.jpg")
 
-    labels_1 = glob.glob(os.path.join(DATA_DIR, "test") + "/*.homographyMatrix")
-    labels_2 = glob.glob(os.path.join(DATA_DIR, "train_val") + "/*.homographyMatrix")
+    matrix_1 = glob.glob(os.path.join(DATA_DIR, "test") + "/*.homographyMatrix")
+    matrix_2 = glob.glob(os.path.join(DATA_DIR, "train_val") + "/*.homographyMatrix")
 
-    images = sorted(images_1 + images_2)
-    labels = sorted(labels_1 + labels_2)
+    images = sorted(images_1 + images_2, key=get_number_from_string)
+    matrices = sorted(matrix_1 + matrix_2, key=get_number_from_string)
 
-    train_images, val_images, train_matrices, val_matrices = train_test_split(
-        images, labels, test_size=0.35
+    if double_input:
+        mask_1 = glob.glob(os.path.join(DATA_DIR, "test") + "/*.mat")
+        mask_2 = glob.glob(os.path.join(DATA_DIR, "train_val") + "/*.mat")
+
+        masks = sorted(mask_1 + mask_2, key=get_number_from_string)
+
+        inputs = [(image, mask) for image, mask in zip(images, masks)]
+    else:
+        inputs = images
+
+    train_input, val_input, train_matrices, val_matrices = train_test_split(
+        inputs, matrices, test_size=0.35
     )
-
-    model = HomographyModel(input_shape=(540, 540, 3))
 
     train_dataset = HomographyDataset(
-        train_images, train_matrices, image_size=(540, 540), batch_size=BATCH_SIZE
+        train_input,
+        train_matrices,
+        double_input=double_input,
+        image_size=(540, 540),
+        batch_size=BATCH_SIZE,
     )
     test_dataset = HomographyDataset(
-        val_images, val_matrices, image_size=(540, 540), batch_size=BATCH_SIZE
+        val_input,
+        val_matrices,
+        double_input=double_input,
+        image_size=(540, 540),
+        batch_size=BATCH_SIZE,
     )
+
+    model = HomographyModel(double_input=double_input, input_shape=(540, 540, 3))
 
     if train:
         model.train(
